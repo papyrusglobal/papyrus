6f491b2381381d3c23796ac94d8acb32496e23a4
Denis <denis@papyrus.global>
2019-04-22 12:37:47 +0200

    Controller UI

--- papyrus-stuff/ui/index.js
+++ papyrus-stuff/ui/index.js
@@ -1,26 +1,30 @@
 const ether = 10 ** 18;
+const biosAddress = '0x0000000000000000000000000000000000000022';
+let contract;
+let account;
 
 async function init() {
   if (typeof web3 === 'undefined') {
     show('no-web3-error');
     return;
   }
-  const w3 = new Web3(web3.currentProvider);
-  const accounts = await w3.eth.getAccounts();
-  const account = accounts[0];
+  web3 = new Web3(web3.currentProvider);
+  const accounts = await web3.eth.getAccounts();
+  account = accounts[0];
   text('account', account);
-  const netId = await w3.eth.net.getId();
+  const netId = await web3.eth.net.getId();
   if (netId != 323138) {
     text('network-id', netId);
     show('no-papyrus-network');
     return;
   }
-  const balance = await w3.eth.getBalance(account);
+  const balance = await web3.eth.getBalance(account);
   text('balance', balance);
   text('balance_eth', balance / ether);
 
-  const contract = new w3.eth.Contract(abi, '0x0000000000000000000000000000000000000022');
+  contract = new web3.eth.Contract(abi, biosAddress);
   text('version', await contract.methods.version().call({ from: account }));
+  text('all-stakes', await web3.eth.getBalance(biosAddress));
   text('stake', await contract.methods.stakes(account).call({ from: account }));
 
   const limit = await request('http://148.251.152.112:52545/', {
@@ -30,6 +34,38 @@ async function init() {
     id: 1
   });
   text('limit', limit.result);
+}
+
+async function onStake() {
+  const value = document.getElementById('value').value;
+  return process(contract.methods.freeze().send({ from: account, gas: 0, value }));
+}
+
+async function onUnstake() {
+  const value = document.getElementById('value').value;
+  return process(contract.methods.melt(value).send({ from: account, gas: 0 }));
+}
+
+async function onWithdraw() {
+  return process(contract.methods.withdraw().send({ from: account, gas: 100000 }));
+}
+
+async function process(transaction) {
+  return transaction
+    .on('receipt', receipt => {
+      console.log('Done!');
+      show('tx-info', false);
+      console.log('Mined: ', receipt.blockNumber);
+    })
+    .on('confirmation', (confirmationNo, receipt) => {
+      console.log('Confirmation', confirmationNo);
+    })
+    .on('transactionHash', hash => {
+      console.log('Tx hash:', hash);
+      text('tx-id', hash);
+      show('tx-info');
+    })
+    .catch(err => console.log('Error:', err.message));
 }
 
 async function request(url, req) {
@@ -42,7 +78,6 @@ async function request(url, req) {
       if (xmlhttp.status !== 200) {
         revert(new Error(xmlhttp.statusText));
       } else {
-        console.log(xmlhttp.responseText.trim());
         resolve(JSON.parse(xmlhttp.responseText));
       }
     };
